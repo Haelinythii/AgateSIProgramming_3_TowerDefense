aeafc9bd40c2884a560f3a07b1bcfa3f511c3062
Haelinythii <dwikiiskandar8@gmail.com>
2021-09-17 16:04:19 +0800

    Create UI for tower

---
+++ Tower Defense/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private static LevelManager instance = null;
+
+    public static LevelManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<LevelManager>();
+
+            }
+            return instance;
+        }
+    }
+
+    [SerializeField] private Transform towerUIParent;
+    [SerializeField] private GameObject towerUIPrefab;
+    [SerializeField] private Tower[] towerPrefabs;
+
+    private void Start()
+    {
+        CreateAllTowerUI();
+    }
+
+    private void CreateAllTowerUI()
+    {
+        foreach (Tower tower in towerPrefabs)
+        {
+            GameObject towerUIGO = Instantiate(towerUIPrefab, towerUIParent);
+            TowerUI towerUI = towerUIGO.GetComponent<TowerUI>();
+            towerUI.SetTowerPrefab(tower);
+            towerUI.transform.name = tower.name;
+        }
+    }
+}
---
+++ Tower Defense/Assets/Scripts/Tower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [Header("Properties")]
+    [SerializeField] private int shootPower = 1;
+    [SerializeField] private float shootDistance = 1f;
+    [SerializeField] private float shootDelay = 5f;
+    [SerializeField] private float bulletSpeed = 1f;
+    [SerializeField] private float bulletSplashRadius = 0f;
+
+    [Header("Reference")]
+    [SerializeField] private SpriteRenderer towerPlace;
+    [SerializeField] private SpriteRenderer towerHead;
+
+    public Sprite GetTowerHeadIcon()
+    {
+        return towerHead.sprite;
+    }
+
+}
